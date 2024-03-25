1a113a436a086a369f15aaa9dfb01c920cd25f8d
octalita <137593388+octalita@users.noreply.github.com>
2024-03-25 23:07:18 +0800

    Add files via upload

---
+++ posttest3/src/Posttest3/Suku_Cadang.java
@@ -0,0 +1,81 @@
+package Posttest3;
+
+public class Suku_Cadang extends kendaraan {
+
+    private String namasukucadang, mereksukucadang, model;
+    private int hargasukucadang, jumlah, tahunsukucadang;
+
+    public Suku_Cadang(String jenis_kendaraan, String namasukucadang, String mereksukucadang, String model, int hargasukucadang, int jumlah,
+            int tahunsukucadang) {
+        super(jenis_kendaraan);
+        this.namasukucadang = namasukucadang;
+        this.mereksukucadang = mereksukucadang;
+        this.model = model;
+        this.hargasukucadang = hargasukucadang;
+        this.jumlah = jumlah;
+        this.tahunsukucadang = tahunsukucadang;
+    }
+
+    // Getter dan setter
+
+    public String getNamasukucadang() {
+        return namasukucadang;
+    }
+
+    public void setNamasukucadang(String namasukucadang) {
+        this.namasukucadang = namasukucadang;
+    }
+
+    public String getMereksukucadang() {
+        return mereksukucadang;
+    }
+
+    public void setMereksukucadang(String mereksukucadang) {
+        this.mereksukucadang = mereksukucadang;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getHargasukucadang() {
+        return hargasukucadang;
+    }
+
+    public void setHargasukucadang(int hargasukucadang) {
+        this.hargasukucadang = hargasukucadang;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public int getTahunsukucadang() {
+        return tahunsukucadang;
+    }
+
+    public void setTahunsukucadang(int tahunsukucadang) {
+        this.tahunsukucadang = tahunsukucadang;
+    }
+
+    void tampil() {
+        System.out.println("Jenis Kendaraan : " + super.getjenis_kendaraan());
+        System.out.println("Nama : " + this.namasukucadang);
+        System.out.println("Merek : " + this.mereksukucadang);
+        System.out.println("Model : " + this.model);
+        System.out.println("Harga : " + this.hargasukucadang);
+        System.out.println("Jumlah: " + this.jumlah);
+        System.out.println("Tahun: " + this.tahunsukucadang);
+    }
+
+   
+    }
+
---
+++ posttest3/src/Posttest3/kendaraan.java
@@ -0,0 +1,21 @@
+package Posttest3;
+
+
+public class kendaraan {
+    String jenis_kendaraan ;
+
+    public kendaraan(String jenis_kendaraan) {
+        this.jenis_kendaraan = jenis_kendaraan;
+        
+    }
+
+    public String getjenis_kendaraan() {
+        return jenis_kendaraan;
+    }
+
+    public void setjenis_kendaraan(String jenis_kendaraan) {
+        this.jenis_kendaraan = jenis_kendaraan;
+   
+}  
+
+}
